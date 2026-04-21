2c7619d3e41af8813dfc48ea9eed56cd117f82e4
roadman <roadmvn@users.noreply.github.com>
2026-04-21 23:29:15 +0000

    feat(calls): add LiveKitClient behaviour

---
+++ lib/whispr_calls/calls/livekit_client.ex
@@ -0,0 +1,18 @@
+defmodule WhisprCalls.Calls.LiveKitClient do
+  @moduledoc false
+  @callback create_room(room_name :: String.t(), opts :: keyword) :: {:ok, map} | {:error, term}
+  @callback delete_room(room_name :: String.t()) :: :ok | {:error, term}
+  @callback generate_access_token(
+              user_id :: String.t(),
+              room_name :: String.t(),
+              opts :: keyword
+            ) ::
+              {:ok, String.t()} | {:error, term}
+
+  def create_room(room, opts), do: impl().create_room(room, opts)
+  def delete_room(room), do: impl().delete_room(room)
+  def generate_access_token(uid, room, opts), do: impl().generate_access_token(uid, room, opts)
+
+  defp impl,
+    do: Application.get_env(:whispr_calls, :livekit_client, WhisprCalls.Calls.LiveKitClientHTTP)
+end
